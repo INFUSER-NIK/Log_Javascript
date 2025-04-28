02a1939fc61c26bfb1a39dc2867620ad9272000e
NIKHIL SINGH <nikhil1108singh@gmail.com>
2025-04-29 01:18:50 +0530

    04

---
+++ variables.js
@@ -0,0 +1,24 @@
+// change in const is not allowed
+const acc_Id = 56849
+
+let acc_Email = "no@gmail.com"
+
+var acc_Passward = "1029"
+// prefer not to use var 
+// bcz of issue in block scope and functional scope
+
+acc_City = "Jaipur"
+
+
+let company_Name
+// if value is not given in the let variable then is assume it as an undefined value
+ 
+
+
+acc_Email = "hc@ha.com"
+acc_Passward = "83484"
+acc_City = "Bengal"
+
+console.log(acc_Id);
+
+console.table([acc_Email,acc_Passward,acc_Id,acc_City,company_Name])
