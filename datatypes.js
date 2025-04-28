a9acbe6de5ccdacf17230d95184224fcba6874e8
NIKHIL SINGH <nikhil1108singh@gmail.com>
2025-04-29 01:17:40 +0530

    03

---
+++ datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser thus it  gives error in vs code
+
+
+console.log("User")
+
+
+let name = "Nexus"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
