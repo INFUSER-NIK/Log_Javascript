d7c21e42bbe1136dbdde28fedf2b3f83feb27428
NIKHIL SINGH <nikhil1108singh@gmail.com>
2025-04-30 01:16:14 +0530

    06

---
+++ compairing_DataTypes.js
@@ -0,0 +1,22 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+//  should always or try to always comapare same data types 
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === (this function is called STRICT CHECK . is strictly checks the value without doing conversion)
+
+console.log("2" === 2);
