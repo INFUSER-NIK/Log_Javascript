c1d23e84e5cea44da59fa256c1ed385f372b495f
NIKHIL SINGH <nikhil1108singh@gmail.com>
2025-04-29 23:44:40 +0530

    05

---
+++ dataType_Conversion.js
@@ -0,0 +1,71 @@
+let score = "Tyrancy"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN (not a number)
+//  true => 1; false => 0
+
+let isLoggedIn = "The_Pathway"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false (empty string)
+// "The_Pathway" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " world" 
+// given space so both the string do not look like linked
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// these type of code output depends upon the priority basis 
+// eg. if there is first is string then all the values will be treated as string then on 
+// and if it is firstly num then its process will be completed then other data type will be executed
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true); => 1
+// console.log(+""); => 0
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
